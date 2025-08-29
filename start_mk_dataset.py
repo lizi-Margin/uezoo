62479262f9567adb2972b75712cf85fdeb31c321
lizi-Margin <sunhaocheng@bupt.edu.cn>
2025-08-29 14:35:53 +0800

    up

--- start_mk_dataset.py
+++ start_mk_dataset.py
@@ -1,3 +1,4 @@
+import re
 import gym
 import cv2
 import json
@@ -9,6 +10,7 @@ import subprocess
 import numpy as np
 from uhtk.imitation.traj import trajectory
 from uhtk.imitation.utils import safe_dump
+from uhtk.siri.utils.sleeper import Sleeper
 from gym_unrealcv.envs.tracking.baseline import PoseTracker, Nav2GoalAgent
 from gym_unrealcv.envs.wrappers import time_dilation, early_done, monitor, augmentation, configUE,agents
 
@@ -74,10 +76,30 @@ def run_bg_genvid(input_dir, fps):
     return process
 
 
+def get_latest_obs(input_dir):
+    max_index = -1
+    max_path = None
+    pattern_png = re.compile(r'^(\d+)_rgb\.png$')
+    for filename in os.listdir(input_dir):
+        if filename.lower().endswith('.png'):
+            match = pattern_png.match(filename)
+            if match:
+                frame_index = int(match.group(1))
+                if frame_index > max_index:
+                    max_path = os.path.join(input_dir, filename)
+                    max_index = frame_index
+    
+    if max_index == -1:
+        assert False, "pattern not match"
+    
+    obs = cv2.imread(max_path)
+    return obs
+
 
 if __name__ == '__main__':
     N_npc = 0
-    fps = 30
+    # fps = 30; dilation = 0.2
+    fps = 15; dilation = 0.4
     episode_time = 10
     SAVE_TRAJ = True
     data_dir = f"./data_dir_{datetime.datetime.now().strftime("%y-%m-%d-%H-%S")}"
@@ -95,12 +117,13 @@ if __name__ == '__main__':
     done = False
     for i in range(episode_count):
         if SAVE_TRAJ:
-            uhtk_traj = trajectory(traj_limit=5000, env_id=0)
-            dict_traj = {}; dict_traj['state_agent_0'] = []; dict_traj['real_time'] = []
+            uhtk_traj = trajectory(traj_limit=500, env_id=0)
+            dict_traj = {}; dict_traj['posrot_agent_0'] = []; dict_traj['real_time'] = []
             for i in range(2 + N_npc): dict_traj[f'action_agent_{i}'] = []
 
         env.seed(i)
-        env.unwrapped.direction = 2*np.pi/8.0 * env.unwrapped.count_eps
+        # env.unwrapped.direction = 2*np.pi/8.0 * env.unwrapped.count_eps
+        env.unwrapped.unrealcv.set_global_time_dilation(dilation)
         obs = env.reset()
         agent_0 = PoseTracker(env.action_space[0], env.unwrapped.reward_params['exp_distance'])
         agent_1 = Nav2GoalAgent(env.action_space[1], env.unwrapped.reset_area, max_len=100)
@@ -118,6 +141,7 @@ if __name__ == '__main__':
             target_to_hide=env.unwrapped.player_list[env.unwrapped.target_id]
         )
         while env.unwrapped.unrealcv.get_record_status(env.unwrapped.cam_list[env.unwrapped.tracker_id]):
+            sleeper = Sleeper(tick=1.0)
             obj_poses = env.unwrapped.obj_poses
             action_0 = agent_0.act(obj_poses[0], obj_poses[1])
             action_1 = agent_1.act(obj_poses[1])
@@ -129,12 +153,17 @@ if __name__ == '__main__':
                 for i, action in enumerate(actions):
                     dict_traj[f'action_agent_{i}'].append(action)
                     uhtk_traj.remember(f'action_agent_{i}', np.array(action))
-
-                tracker_pos = env.unwrapped.unrealcv.get_obj_location(env.unwrapped.player_list[0])
-                tracker_rot = env.unwrapped.unrealcv.get_obj_rotation(env.unwrapped.player_list[0])
+                
+                [tracker_pos, tracker_rot] = env.unwrapped.unrealcv.batch_cmd(
+                    [env.unwrapped.unrealcv.get_obj_location(env.unwrapped.player_list[0], return_cmd=True), env.unwrapped.unrealcv.get_obj_rotation(env.unwrapped.player_list[0], return_cmd=True)],
+                    [env.unwrapped.unrealcv.decoder.string2floats, env.unwrapped.unrealcv.decoder.string2floats]
+                )
                 Pose = tracker_pos + tracker_rot
-                dict_traj['state_agent_0'].append(Pose)
-                uhtk_traj.remember('state_agent_0', np.array(Pose))
+                tracker_FRAME = get_latest_obs(episode_dir)
+                uhtk_traj.remember('FRAME_obs_agent_0', tracker_FRAME)
+                
+                dict_traj['posrot_agent_0'].append(Pose)
+                uhtk_traj.remember('posrot_agent_0', np.array(Pose))
 
                 real_time = time.time() - t0
                 dict_traj['real_time'] = real_time
@@ -143,14 +172,15 @@ if __name__ == '__main__':
                 uhtk_traj.time_shift()
 
             count_step += 1
-            time.sleep(0.1)
+            sleeper.sleep()
 
         fps_ = (fps * episode_time) / (time.time() - t0)
         print ('推算渲染速度相对真实时间Fps (仅参考):' + str(fps_))
-        with open(f"{episode_dir}/dict_traj.json", 'w') as f:
-            json.dump(dict_traj, f, indent=2)
-        uhtk_traj.cut_tail()
-        safe_dump(uhtk_traj, f"{episode_dir}/traj-hw-0.d")
+        if SAVE_TRAJ:
+            with open(f"{episode_dir}/dict_traj.json", 'w') as f:
+                json.dump(dict_traj, f, indent=2)
+            uhtk_traj.cut_tail()
+            safe_dump(uhtk_traj, f"{episode_dir}/traj-hw-0.d")
             
         run_bg_genvid(episode_dir, fps)
 
